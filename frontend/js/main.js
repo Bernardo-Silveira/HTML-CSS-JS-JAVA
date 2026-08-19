43a8be963019725f152bdf0e494479dbdf45c489
Bernardo Silveira <bernardosilveira2022@gmail.com>
2026-08-19 18:36:14 -0300

    A new files for project and a some modify in frontend

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,7 +1,38 @@
-console.log("JavaScript working!");
+document
+  .getElementById("formCadastro")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
 
-fetch("http://localhost:8080/api/test")
-  .then((response) => response.text())
-  .then((data) => {
-    console.log(data);
+    const name = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const mensageDiv = document.getElementById("mensagem");
+
+    const data = {
+      name: name,
+      email: email,
+    };
+
+    try {
+      const response = await fetch("http://localhost:8080/api/clients", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        const respostaTexto = await response.text();
+        mensageDiv.className = "alert alert-success mt-3";
+        mensageDiv.textContent = respostaTexto;
+        mensageDiv.classList.remove("d-none");
+        document.getElementById("formRegister").reset();
+      } else {
+        throw new Error("Error processor request of server.");
+      }
+    } catch (error) {
+      mensageDiv.className = "alert alert-danger mt-3";
+      mensageDiv.textContent = "Error of conection with server.";
+      mensageDiv.classList.remove("d-none");
+    }
   });
