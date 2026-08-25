74ed9e12481f4e42c9e12f2efe99fa89d10252dc
Bernardo Silveira <bernardosilveira2022@gmail.com>
2026-08-25 16:46:51 -0300

    Test and modify frontend, backend

--- backend/src/main/java/com/example/api/controller/ClientController.java
+++ backend/src/main/java/com/example/api/controller/ClientController.java
@@ -1,21 +1,25 @@
 package com.example.api.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
 import com.example.api.model.Client;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/api/clients")
+@CrossOrigin
 public class ClientController {
 
     @PostMapping
-    public Client createClient(@RequestBody Client client) {
-
-        System.out.println("Nome: " + client.getName());
+    public Client postMethodName(@RequestBody Client client) {
+        System.out.println("Name: " + client.getName());
         System.out.println("Email: " + client.getEmail());
 
         return client;
     }
+    
 }
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -5,7 +5,8 @@ document
 
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
-    const mensageDiv = document.getElementById("mensage");
+
+    const messageDiv = document.getElementById("message");
 
     const data = {
       name: name,
@@ -23,16 +24,16 @@ document
 
       if (response.ok) {
         const responseText = await response.text();
-        mensageDiv.className = "alert alert-success mt-3";
-        mensageDiv.textContent = responseText;
-        mensageDiv.classList.remove("d-none");
+        messageDiv.className = "alert alert-success mt-3";
+        messageDiv.textContent = responseText;
+        messageDiv.classList.remove("d-none");
         document.getElementById("formRegister").reset();
       } else {
         throw new Error("Error processor request of server.");
       }
     } catch (error) {
-      mensageDiv.className = "alert alert-danger mt-3";
-      mensageDiv.textContent = "Error of conection with server.";
-      mensageDiv.classList.remove("d-none");
+      messageDiv.className = "alert alert-danger mt-3";
+      messageDiv.textContent = "Error of conection with server.";
+      messageDiv.classList.remove("d-none");
     }
   });
