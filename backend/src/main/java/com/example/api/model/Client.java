4a7751d2793bb4e2927dd9766d372662a9c6d504
Bernardo Silveira <bernardosilveira2022@gmail.com>
2026-08-25 16:05:25 -0300

    Upgraded and modified too file for backends

--- backend/src/main/java/com/example/api/model/Client.java
+++ backend/src/main/java/com/example/api/model/Client.java
@@ -1,5 +1,26 @@
 package com.example.api.model;
 
 public class Client {
-    
+
+    private String name;
+    private String email;
+
+    public Client() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
